ed5a6a8c91ec4d14cce9e9032e23114f1eaef0e1
derek19960312 <46845914+derek19960312@users.noreply.github.com>
2019-03-23 02:59:17 +0800

    setback COlor2

--- app/src/main/java/weshare/groupfour/derek/MainActivity.java
+++ app/src/main/java/weshare/groupfour/derek/MainActivity.java
@@ -1,6 +1,8 @@
 package weshare.groupfour.derek;
 
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,6 +11,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import weshare.groupfour.derek.Commondity.Commondity_Browse;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         toolbar = findViewById(R.id.toolbar);
         dlMain = findViewById(R.id.dlMain);
@@ -53,24 +58,14 @@ public class MainActivity extends AppCompatActivity{
             }
         });
     }
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.search_menu, menu);
-//
-//        MenuItem menuSearchItem = menu.findItem(R.id.my_search);
-//
-//        // Get the SearchView and set the searchable configuration
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView = (SearchView) menuSearchItem.getActionView();
-//
-//        // Assumes current activity is the searchable activity
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-//
-//        // 這邊讓icon可以還原到搜尋的icon
-//        searchView.setIconifiedByDefault(true);
-//        return true;
-//    }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu,menu);
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
 }
