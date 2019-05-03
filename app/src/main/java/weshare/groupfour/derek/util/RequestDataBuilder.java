b6a37cb27980fe09ad836ba91cf20e31e1e8ccfb
derek19960312 <46845914+derek19960312@users.noreply.github.com>
2019-05-03 10:55:19 +0800

    0503

---
+++ app/src/main/java/weshare/groupfour/derek/util/RequestDataBuilder.java
@@ -0,0 +1,29 @@
+package weshare.groupfour.derek.util;
+
+public class RequestDataBuilder {
+
+    private StringBuilder sb;
+
+
+    public RequestDataBuilder build(){
+        sb = new StringBuilder();
+        return new RequestDataBuilder();
+    }
+
+    public RequestDataBuilder setAction(String action){
+        sb.append("action="+action+"&");
+        return this;
+    }
+
+    public RequestDataBuilder setData(String key,String val){
+        sb.append(key+"="+val+"&");
+        return this;
+    }
+
+
+
+    public String create(){
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+}
